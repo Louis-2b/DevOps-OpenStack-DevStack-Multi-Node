bdbb5d10cd5af8850b75ab05a70d69508e8e2c76
LOUIS STEPHANE <93147687+Louis-2b@users.noreply.github.com>
2025-10-20 10:43:21 +0200

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -27,45 +27,49 @@ openstack role add admin --user $NAME --project $PROJECT
 
 ############################################################################################################
 
-# Module Terraform pour déployer un bastion DGFIP sur OpenStack.
-# Ce module est versionné (ref=v2.1.1) pour garantir la reproductibilité.
-# ⚠️ Toujours fixer la version du module pour éviter les changements inattendus.
+# --- Module Terraform pour déployer un HAproxy DGFIP sur OpenStack ----
 
-module "dgfip_bastion_outillage" {
+
+module "dgfip_haproxy_outillage" {
   # Source du module : dépôt Git interne DGFIP avec version figée
-  source = "git::https://forge.dgfip.finances.rie.gouv.fr/dgfip/si1/dan-a2c/module-terraform-dgfip/s-curit/terraform-openstack-bastion.git?ref=v2.1.1"
+  source                       = "git::https://forge.dgfip.finances.rie.gouv.fr/dgfip/si1/dan-a2c/module-terraform-dgfip/networking/terraform-openstack-haproxy.git?ref=v2.3.0"
 
-  # Nom de l'image (système d’exploitation) utilisée pour le bastion.
-  # ⚙️ À paramétrer via une variable pour faciliter le changement d’image selon l’environnement.
-  image_name = var.image_name
+  # Nom de l'image (système d’exploitation) utilisée pour le haproxy
+  # À paramétrer via une variable pour faciliter le changement d’image selon l’environnement
+  image_name                   = var.haproxy_image_name
 
-  # Préfixe du projet ou de la plateforme.
-  # 🧱 Permet de distinguer les environnements (ex : pf_prefixe = "a2c-dev", "a2c-prod", etc.)
-  pf_prefixe = var.pf_prefixe
+  # Préfixe du projet ou de la plateforme
+  # Permet de distinguer les environnements
+  pf_prefixe                   = var.pf_prefixe
 
-  # Phase du déploiement (ex : dev, recette, prod)
-  # 🧩 Facilite la gestion des ressources multi-environnements.
-  phase = var.phase
+  # Phase du déploiement
+  # Facilite la gestion des ressources multi-environnements
+  phase                        = var.phase
+
+
+  server_count                 = var.haproxy_server_count
 
   # Type de machine OpenStack (CPU/RAM)
-  # 💡 Choisir des flavors adaptés à la charge attendue.
-  flavor_name = var.bastion_flavor_name
+  # Choisir des flavors adaptés à la charge attendue.
+  flavor_name                  = var.haproxy_flavor_name
 
-  # Paire de clés SSH autorisée pour l’accès administrateur au bastion
-  # 🔐 À générer et gérer en dehors du module pour éviter de stocker des clés dans le code.
-  key_pair = var.key_pair
+  # Paire de clés SSH autorisée pour l’accès administrateur au haproxy
+  # À générer et gérer en dehors du module pour éviter de stocker des clés dans le code
+  key_pair                     = var.key_pair
 
-  # Réseau d’administration interne où le bastion sera connecté
-  admin_network_id = data.openstack_networking_network_v2.admin_network.id
+  # Réseau d’administration interne où le haproxy sera connecté
+  admin_network_id             = data.openstack_networking_network_v2.admin_network.id
 
-  # Pool d’adresses IP flottantes pour l’accès externe (public)
-  admin_floating_ip_pool = var.admin_external_network_name
+  # Règles de sécurité spécifiques au haproxy
+  # Définies dans un objet variable pour gérer finement les accès (SSH, ICMP, etc.)
+  admin_sg_haproxy_rules       = var.admin_sg_haproxy_rules
 
-  # IP flottante fixe à réutiliser (si existante)
-  # 🧭 Permet la redéployabilité sans changer d’adresse IP.
-  admin_fixed_fip = [data.openstack_networking_floatingip_v2.bastion_ip.address]
-
-  # Règles de sécurité spécifiques au bastion
-  # 🔒 Définies dans un objet variable pour gérer finement les accès (SSH, ICMP, etc.)
-  admin_sg_bastion_rules = var.admin_sg_bastion_rules
+  
+  pub_network_id               = data.openstack_networking_network_v2.pub_network.id
+  publication_floating_ip_pool = var.pub_external_network_name
+  publication_fixed_fip        = [data.openstack_networking_floatingip_v2.haproxy_ip.address]
+  servergroup_az1_id           = var.servergroup_az1_id
+  servergroup_az2_id           = var.servergroup_az2_id
+  vips_pub                     = var.haproxy_vips_pub
+  extra_disks                  = var.haproxy_extra_disks
 }
